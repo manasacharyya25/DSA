3cf1a3e120235cde7a5fa0056afc1e25ce5bbf59
Manas Pratim Acharyya <manaspratim.acharyya@philips.com>
2019-10-22 14:40:34 +0530

    Added DP Problems

---
+++ Dynamic Programming/01. Cutted Segments.py
@@ -0,0 +1,32 @@
+class Solution:
+    @staticmethod
+    def get_max_cut_segments(n, arr):
+        N = [i for i in range(n+1)]
+        arr.insert(0,0)
+
+        dp = [[0 for j in range(len(N))] for i in range(len(arr))]
+
+        for i in range(1, len(arr)):
+            for j in range(1, n+1):
+                if arr[i] > N[j]:
+                    dp[i][j] = dp[i-1][j]
+                else:
+                    if ( j-arr[i] == 0 ) or (dp[i][j-arr[i]] != 0):
+                        dp[i][j] = max(1 + dp[i][j-arr[i]], dp[i-1][j])
+                    else:
+                        dp[i][j] = dp[i-1][j]
+
+        print(dp[-1][-1])
+
+if __name__=="__main__":
+    t = int(input())
+
+    while t > 0:
+        t-=1
+
+        n = int(input())
+
+        arr = list(map(int, input().strip().split()))
+
+        Solution.get_max_cut_segments(n, arr)
+
